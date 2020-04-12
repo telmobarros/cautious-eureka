08aca9d1b7928f185298aff78d925a270b7a7283
Telmo Barros <telmo.20@gmail.com>
2020-04-12 17:12:26 +0100

    fonts added, logo + routing

--- src/App.js
+++ src/App.js
@@ -1,77 +1,78 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useLocation
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import { Button } from 'react-bootstrap';
+import { IoIosSend } from 'react-icons/io';
+
+import { Container, Row, Button, Col, Form } from 'react-bootstrap';
+
+function Navbar() {
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="navbar navbar-expand fixed-top py-3">
+      <div className="collapse navbar-collapse w-100">
+        <ul className="navbar-nav mr-auto">
+          {pathname !== '/' && pathname !== '/works' &&
+            <li className="nav-item">
+              <Link to="/works" className="nav-link">Works</Link>
+            </li>
+          }
+          {pathname !== '/' && pathname !== '/bio' && pathname === '/works' &&
+            <li className="nav-item">
+              <Link to="/bio" className="nav-link">Bio</Link>
+            </li>
+          }
+        </ul>
+      </div>
+      <div className="mx-auto">
+        <Link to="/" className="navbar-brand mx-auto d-none d-lg-inline-block">NEURA of the NORTH</Link>
+        <Link to="/" className="navbar-brand mx-auto d-lg-none d-inline-block">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className="navbar-collapse collapse w-100">
+        <ul className="navbar-nav ml-auto">
+          {pathname !== '/' && pathname !== '/bio' && pathname === '/contact' &&
+            <li className="nav-item">
+              <Link to="/bio" className="nav-link">Bio</Link>
+            </li>
+          }
+          {pathname !== '/' && pathname !== '/contact' &&
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </li>
+          }
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 function App() {
   return (
     <div className="App" >
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a className="navbar-brand" href="#">RAFAEL MAIA</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          <form className="form-inline mt-2 mt-md-0">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"></input>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li><Link to="/">
-                  <Button variant="primary">Home</Button>
-                </Link>
-
-                </li>
-                <li>
-                  <Link to="/works">Works</Link>
-                </li>
-                <li>
-                  <Link to="/about">Bio</Link>
-                </li>
-              </ul>
-            </nav>
-
-            {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/works">
-                <Works />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route path="/works">
+          <Works />
+        </Route>
+        <Route path="/bio">
+          <Bio />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
@@ -110,7 +111,7 @@ class Home extends React.Component {
     // store intervalId in the state so it can be accessed later:
     this.setState({ intervalId: intervalId });
 
-    fetch('https://rafaelmaia.000webhostapp.com/wp-json/wp/v2/events', /*{
+    fetch('https://neuranorth.000webhostapp.com/wp-json/wp/v2/events', /*{
       method: 'GET',
       mode: 'no-cors',
       withCredentials: true,
@@ -159,8 +160,8 @@ class Home extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <div>
-          <h2>Events</h2>
+        <section>
+          {/*<h2>Events</h2>
           <div className="scene">
             <div className="carousel" style={{ transform: 'translateZ(-120px) rotateX(' + (selectedIndex / cellCount) * -360 + 'deg)' }}>
               {events.map((value, index) => {
@@ -180,15 +181,120 @@ class Home extends React.Component {
           <p style={{ textAlign: "center" }}>
             <button className="previous-button" onClick={() => this.previousEvent()}>Previous</button>
             <button className="next-button" onClick={() => this.nextEvent()}>Next</button>
-          </p>
-        </div >
+          </p>*/}
+          <div>
+            <Link to="/works">Works</Link>
+            <Link to="/bio">Bio</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+          <footer className="social-container">
+            <div className="mx-auto">
+              <a href="https://www.facebook.com/profile.php?id=100000174129300" target="_blank" ><span>FACEBOOK</span></a>
+              <span className="slash"> / </span>
+              <a href="https://www.instagram.com/neuraofthenorth/" target="_blank"><span>INSTAGRAM</span></a>
+              <span className="slash"> / </span>
+              <a href="https://www.youtube.com/user/rafael87909" target="_blank"><span>YOUTUBE</span></a>
+            </div>
+          </footer>
+        </section >
       );
     }
   }
 }
 
-function About() {
-  return <h2>About</h2>;
+function Bio() {
+  return <h2>Bio</h2>;
+}
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    let formData = new FormData();
+
+    formData.append('your-name', this.state.name);
+    formData.append('your-email', this.state.email);
+    formData.append('your-subject', this.state.subject);
+    formData.append('your-message', this.state.message);
+
+    fetch('https://neuranorth.000webhostapp.com/wp-json/contact-form-7/v1/contact-forms/43/feedback', {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.json())
+      .then(
+        (res) => {
+          console.log('ok')
+          console.log(res.status)
+          if (res.status === 'validation_failed') {
+            this.setState({ error: res })
+            console.log(this.state)
+          }
+          console.log(res)
+        },
+        (error) => {
+          console.log('erro')
+          console.log(error)
+        }
+      )
+    for (var value of formData.values()) {
+      console.log(value);
+    }
+    event.preventDefault();
+  }
+
+  render() {
+    const { error, name, email, subject, message } = this.state;
+    return (
+      <section>
+        <Container fluid>
+          <Row>
+            <Col xs="12" sm="10" md="8" lg="4" className="mx-auto">
+              <p className="font-weight-bolder mb-0">MAIL ME</p>
+              <a className="nostyle" href={'mailto:rafamaianet@hotmail.com?subject=' + subject + '&body=' + message}>rafamaianet@hotmail.com</a>
+              <p className="my-4">- or -</p>
+              <p className="font-weight-bolder mb-0">FILL THE FORM BELOW</p>
+              {error &&
+                <p className="small text-danger error-message">{error.message}</p>
+              }
+              <Form className="contact-form" onSubmit={this.handleSubmit}>
+                <Form.Group>
+                  <Form.Control type="text" placeholder="Name" name="name" value={name} onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control type="email" placeholder="name@example.com" name="email" value={email} onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control type="text" placeholder="Subject" name="subject" value={subject} onChange={this.handleChange} />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control as="textarea" rows="5" placeholder="Tell me more..." name="message" value={message} onChange={this.handleChange} />
+                </Form.Group>
+                <Button variant="dark" type="submit">SEND <IoIosSend /></Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section >
+    );
+  }
+
 }
 
 class Works extends React.Component {
@@ -203,7 +309,7 @@ class Works extends React.Component {
 
   componentDidMount() {
 
-    fetch('https://rafaelmaia.000webhostapp.com/wp-json/wp/v2/projects')
+    fetch('https://neuranorth.000webhostapp.com/wp-json/wp/v2/projects')
       .then(res => res.json())
       .then(
         (projects) => {
@@ -231,14 +337,14 @@ class Works extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <div>
+        <section>
           <h2>Works</h2>
           <ul>
             {projects.map((value, index) => {
               return <li>{value.title.rendered}</li>
             })}
           </ul>
-        </div>
+        </section>
       );
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-  
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
